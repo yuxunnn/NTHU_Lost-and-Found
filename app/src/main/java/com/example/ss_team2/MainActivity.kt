f5502c296f1d6bb0ea325d6f589c00e511788d8a
Using <t555555561@gmail.com>
2022-05-25 21:28:57 +0800

    WhatYouLost page

--- app/src/main/java/com/example/ss_team2/MainActivity.kt
+++ app/src/main/java/com/example/ss_team2/MainActivity.kt
@@ -11,33 +11,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.ss_team2.ui.theme.SSteam2Theme
+import com.example.ss_team2.whatYouLost.WhatYouLost
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            SSteam2Theme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            WhatYouLost()
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    SSteam2Theme {
-        Greeting("Android")
     }
 }
