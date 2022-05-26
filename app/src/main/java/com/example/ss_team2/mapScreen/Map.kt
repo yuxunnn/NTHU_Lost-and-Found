5e3a806a24ab724c996cc952157874bea12a11b8
yuxunnn <yuxun90140@gmail.com>
2022-05-26 16:30:21 +0800

    map test

--- app/src/main/java/com/example/ss_team2/mapScreen/Map.kt
+++ app/src/main/java/com/example/ss_team2/mapScreen/Map.kt
@@ -1,26 +1,22 @@
 package com.example.ss_team2.mapScreen
 
-import androidx.compose.foundation.layout.Box
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.Marker
+import com.google.maps.android.compose.rememberCameraPositionState
+
 
 @Composable
 fun Map(modifier: Modifier) {
-    Box(modifier = modifier) {
-
+    val singapore = LatLng(1.35, 103.87)
+    val cameraPositionState = rememberCameraPositionState {
+        position = CameraPosition.fromLatLngZoom(singapore, 10f)
     }
-//    val singapore = LatLng(1.35, 103.87)
-//    val cameraPositionState = rememberCameraPositionState {
-//        position = CameraPosition.fromLatLngZoom(singapore, 10f)
-//    }
-//    GoogleMap(
-//        modifier = Modifier.fillMaxSize(),
-//        cameraPositionState = cameraPositionState
-//    ) {
-//        Marker(
-//            state = MarkerState(position = singapore),
-//            title = "Singapore",
-//            snippet = "Marker in Singapore"
-//        )
-//    }
+    GoogleMap(
+        modifier = modifier,
+        cameraPositionState = cameraPositionState
+    )
 }
