f88fa566a147296aa237d3f852c5caab37152147
109062113 <109062113>
2022-06-01 23:57:36 +0800

    navi

--- app/src/main/java/com/example/ss_team2/RyanJetpack/AddLostList.kt
+++ app/src/main/java/com/example/ss_team2/RyanJetpack/AddLostList.kt
@@ -1,42 +1,30 @@
-package com.example.ss_team2
+package com.example.ss_team2.RyanJetpack
 
 
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.Icon
 import androidx.compose.material.icons.filled.*
-import androidx.compose.material.icons.materialIcon
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
-import androidx.compose.ui.graphics.Shape
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.ss_team2.AnonymousUserCard
+import com.example.ss_team2.EditItemCard
+import com.example.ss_team2.R
 import com.example.ss_team2.ui.theme.SSteam2Theme
-import com.example.ss_team2.ui.theme.Shapes
-import java.lang.ProcessBuilder.Redirect.to
 
 @Composable
 fun AddLostListHomeScreen(
