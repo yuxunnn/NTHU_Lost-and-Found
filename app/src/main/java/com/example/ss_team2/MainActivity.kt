cd3c3626209856f0d2b85c2267dac30acd208a33
Using <t555555561@gmail.com>
2022-06-02 17:16:43 +0800

    Welcome and RegisterPage

--- app/src/main/java/com/example/ss_team2/MainActivity.kt
+++ app/src/main/java/com/example/ss_team2/MainActivity.kt
@@ -3,16 +3,13 @@ package com.example.ss_team2
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.ui.Modifier
-import com.example.ss_team2.ui.theme.SSteam2Theme
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.*
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            SSteam2Theme {
+            /*SSteam2Theme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
@@ -20,7 +17,7 @@ class MainActivity : ComponentActivity() {
                 ) {
 
                 }
-            }
+            }*/
         }
     }
 }
