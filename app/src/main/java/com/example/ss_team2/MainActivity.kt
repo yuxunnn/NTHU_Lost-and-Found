20c7e17a4c8ae7a46882fce1c46eb70b5434cafc
zun <1741954267@qq.com>
2022-06-04 01:58:33 +0800

    test

--- app/src/main/java/com/example/ss_team2/MainActivity.kt
+++ app/src/main/java/com/example/ss_team2/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.example.ss_team2
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-
+import androidx.lifecycle.lifecycleScope
+import com.apollographql.apollo3.ApolloClient
+import com.example.ss_team2.UserByIdQuery
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,6 +21,16 @@ class MainActivity : ComponentActivity() {
 
                 }
             }*/
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        val apolloClient = ApolloClient.Builder()
+            .serverUrl("http://10.0.2.2:3000/graphql").build()
+        lifecycleScope.launchWhenResumed {
+            val response = apolloClient.query(UserByIdQuery("1")).execute()
+            Log.d("Graphql", "Success: ${response.data}")
         }
     }
 }
