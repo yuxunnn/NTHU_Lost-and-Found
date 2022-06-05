bc835f72330b634270eb410e12ae21c63d4fb162
Using <t555555561@gmail.com>
2022-06-06 01:14:51 +0800

    Ranking ShopItem Post UseCase

--- app/src/main/java/com/example/ss_team2/presentation/viewModel/PostViewModel.kt
+++ app/src/main/java/com/example/ss_team2/presentation/viewModel/PostViewModel.kt
@@ -1,2 +1,40 @@
 package com.example.ss_team2.presentation.viewModel
 
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.ss_team2.domain.model.Post
+import com.example.ss_team2.domain.use_case.PostUseCase
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+
+class PostViewModel(): ViewModel(){
+
+    private val postUseCase = PostUseCase()
+
+    private val _posts = MutableStateFlow(postFrog)
+    val posts = StateFlow<Post> = _posts,
+
+    init{
+
+    }
+
+     fun createPost(){
+         viewModelScope.launch{
+             postUseCase.createPost(
+                Post(
+                    image = ,
+                    type = ,
+                    what = ,
+                    where = ,
+                    description = ,
+                    done = ,
+                    bounty = ,
+                    time = ,
+                    owner = ,
+                )
+             )
+         }
+     }
+}
--- app/src/main/java/com/example/ss_team2/presentation/viewModel/UserViewModel.kt
+++ app/src/main/java/com/example/ss_team2/presentation/viewModel/UserViewModel.kt
@@ -1,2 +1,3 @@
 package com.example.ss_team2.presentation.viewModel
 
+
