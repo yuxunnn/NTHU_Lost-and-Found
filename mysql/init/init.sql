86976c2ac971cbbe32c59cade125774b4fbbaf40
yuxunnn <yuxun90140@gmail.com>
2023-06-24 01:50:14 +0800

    sql

--- mysql/init/init.sql
+++ mysql/init/init.sql
@@ -1,7 +1,3 @@
 USE ssteam2;
 
-source /opt/sql/customer.sql;
-source /opt/sql/ordered.sql;
-source /opt/sql/box.sql;
-source /opt/sql/product.sql;
-source /opt/sql/relation.sql;
+source /opt/sql/user.sql;
--- mysql/sql/user.sql
+++ mysql/sql/user.sql
@@ -1,7 +1,5 @@
-CREATE TABLE customer (
+CREATE TABLE user (
 	id int AUTO_INCREMENT PRIMARY KEY,
     person_name varchar(10),
-	phone varchar(20),
-    address varchar(30),
-    relation varchar(10)
+	phone varchar(20)
 );
